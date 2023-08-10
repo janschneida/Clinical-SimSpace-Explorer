c729753c793e8cac941b83c2f06e081a4f76b25e
Jan Schneider <36661966+janschneida@users.noreply.github.com>
2023-08-10 13:55:53 +0200

    clean up

--- create_local_application_resources.py
+++ create_local_application_resources.py
@@ -3,7 +3,6 @@ import src.data_utils as du
 import pandas as pd
 import numpy as np
 import csv
-#paac_msk_jco_2023, acbc_mskcc_2015,acyc_jhu_2016
 
 def safe_panther_terms(mut_dict):
     
@@ -80,26 +79,5 @@ def safe_gene_sim_look_up(genes,term_dict):
 
 
 if __name__ == '__main__':
-    # bcc has 16667 genes -> should be enough I guess LOL
-    mut_dict = du.getMutationProfileDictFromStudy('bcc_unige_2016')#paac_msk_jco_2023, acbc_mskcc_2015,acyc_jhu_2016
-    # safe_uniprot_lookup(mut_dict)
-    # safe_oncoKB_lookup()
-    # safe_panther_terms(mut_dict)
-    # safe_pygosemsim_terms(mut_dict)
-    # safe_cbio_study('acbc_mskcc_2015')
-    # du.__initOncoKBLookUp__()
-    # genes = du.oncoKB_lookup
-    genes = du.getUniqueMutations(mut_dict)
-    term_dict = du.__initPantherDBtermDict__(genes[:10])
-    # NOTE we can right to new line easily withour overriden eversrhing
-    # safe_gene_sim_look_up(genes[:1000],term_dict)
-    # safe_cbio_study('blca_nmibc_2017')
-    # safe_cbio_study('chol_msk_2018')
-    # safe_cbio_study('breast_alpelisib_2020')
-    # safe_cbio_study('brca_jup_msk_2020')
-    # safe_cbio_study('brca_mapk_hp_msk_2021')
-    # safe_cbio_study('crc_nigerian_2020')
-    # safe_cbio_study('crc_dd_2022')
-    # safe_cbio_study()
-    # safe_cbio_study()
+    return
     
